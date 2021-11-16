08da68b398c99a50f26c1662a0d6f5a07e0dca20
Mailen Bonavita <mailenbonavita@gmail.com>
2021-11-16 08:37:25 -0300

    problemas con rutas

--- consultorio/config/routes.rb
+++ consultorio/config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:"professionals#index" #ruta de inicio
-  get "professionals", to:"professionals#index"
+  get "/professionals", to:"professionals#index"
 
-  get 'professionals/:id/edit', to: 'professionals#edit'
-  patch 'professionals/:id', to: 'professionals#update', as: :professional
+  get '/professionals/:id/edit', to: 'professionals#edit'
+  patch '/professionals/:id', to: 'professionals#update', as: :professional
 
-  get 'professionals/new', to: 'professionals#new'
-  post "professionals", to:'professionals#create'
+  get '/professionals/new', to: 'professionals#new'
+  post "/professionals", to:'professionals#create'
 
-  #get 'professionals/:id', to: 'professionals#show'
-  delete 'professionals/:id', to: 'professionals#destroy'
+  get '/professionals/:id', to: 'professionals#show'
+  delete '/professionals/:id', to: 'professionals#destroy'
 
 
 
