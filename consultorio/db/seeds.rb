7eba45e3f029cab5aae661b5a743f5dbbdaaacf6
Mailen Bonavita <mailenbonavita@gmail.com>
2021-12-14 19:38:56 -0300

    Arreglo del seed

--- consultorio/db/seeds.rb
+++ consultorio/db/seeds.rb
@@ -6,6 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'admin@gmail.com', password: '123456', role:2)
-User.create(email: 'asistencia@gmail.com', password: '123456', role:1)
-User.create(email: 'consulta@gmail.com', password: '123456')
+#usuarios para ingresar al sistema
+User.create(email: 'admin@gmail.com', password: '123456', role:"administrator")
+User.create(email: 'asistencia@gmail.com', password: '123456', role:"assistance")
+User.create(email: 'consulta@gmail.com', password: '123456', role:"query")
+
+#profesionales
+p1=Professional.create(name:"Marina Baez")
+p2=Professional.create(name:"Luciano Ramirez")
+p3=Professional.create(name:"Alfonso Vazquez")
+p4=Professional.create(name:"Lucia Lopetegui")
+
+#turnos
+Appointment.create(date:"2021-12-29 10:30", name:"Graciela", professional_id:p1.id, surname:"Perez", phone:"1123213")
+Appointment.create(date:"2022-01-03 11:00", name:"Alberto", professional_id:p2.id, surname:"Garcia", phone:"1123213")
+Appointment.create(date:"2022-01-03 10:30", name:"Lujan", professional_id:p2.id, surname:"LInares", phone:"1123213")
+Appointment.create(date:"2021-12-29 10:30", name:"Pedro", professional_id:p3.id, surname:"Perez", phone:"1123213")
+Appointment.create(date:"2021-12-29 12:30", name:"Lautaro", professional_id:p3.id, surname:"Carmona", phone:"1123213")
+Appointment.create(date:"2022-01-03 10:30", name:"Martina", professional_id:p3.id, surname:"Giles", phone:"1123213")
